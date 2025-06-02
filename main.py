807236a8be21c7526d8f9392e801880610b00890
nasdj21 <nasierra@espol.edu.ec>
2025-06-02 15:01:41 -0500

    Se mejoro el estandar del codigo

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+"""Función main del codigo de gimnasio
+
+    Returns:
+        ejecución del codigo
+"""
 from membership import get_plan_details
 from calculator import calculate_total_cost
 
-
 def main():
-    # === Requisito 8: User Confirmation ===
+    """Requisito 8: User Confirmation"""
     plan_name = input("Seleccione plan (Basic, Premium, Family): ")
     plan = get_plan_details(plan_name)
 
-    # === Requisito 10: Error Handling (plan inválido) ===S
+    # === Requisito 10: Error Handling (plan inválido) ===
     if not plan:
         print("Plan no válido.")
         return -1  # === Requisito 9: Return -1 si la entrada no es válida ===
@@ -30,7 +34,7 @@ def main():
     total = calculate_total_cost(plan, selected_features, group_members)
 
     # === Requisito 8: Mostrar resumen de selección al usuario ===
-    print(f"\nResumen del plan:")
+    print("\nResumen del plan:")
     print(f"Plan: {plan_name}")
     print(f"Características: {', '.join(selected_features) if selected_features else 'Ninguna'}")
     print(f"Miembros: {group_members}")
@@ -40,7 +44,5 @@ def main():
     if confirm.lower() == 's':
         print("¡Membresía registrada con éxito!")
         return total  # === Requisito 9: Return de costo si se confirma
-    else:
-        print("Membresía cancelada.")
-        return -1  # === Requisito 9: Return -1 si se cancela
-    
+    print("Membresía cancelada.")
+    return -1  # === Requisito 9: Return -1 si se cancela
